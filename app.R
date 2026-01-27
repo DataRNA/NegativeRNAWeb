0068a8b988c05f3568318cdaac6fd598e7ff02b4
Mehmeteminorhan <mehmeteminorhan99@gmail.com>
2026-01-27 10:28:44 +0300

    NeRNAWeb v1.0

--- app.R
+++
@@ -1,9 +0,0 @@
-# Gerekli kütüphaneleri yükle
-source("global.R")
-
-# UI ve Server dosyalarını yükle
-source("ui.R")
-source("server.R")
-
-# Uygulamayı çalıştır
-shinyApp(ui = ui, server = server)
